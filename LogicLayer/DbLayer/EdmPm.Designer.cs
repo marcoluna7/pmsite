0b7294b6cf2f3d7967b3c8c66a54219f871eb133
marcoluna7 <markorex7@gmail.com>
2013-09-19 23:24:54 -0500

    borramos
    todo

--- LogicLayer/DbLayer/EdmPm.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// La generación de código predeterminada está deshabilitada para el modelo 'C:\Users\W530\Documents\Visual Studio 2012\Projects\PmWebSite\LogicLayer\DbLayer\EdmPm.edmx'. 
-// Para habilitar la generación de código predeterminada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
-// por otro valor. Esta propiedad está disponible en la ventana Propiedades cuando se abre
-// el modelo en el diseñador.
